95f210b3bdbf69ab874628f9862ff142211636f6
Brian Alexis <51463930+brrianalexis@users.noreply.github.com>
2020-10-14 23:46:35 -0300

    feat: update me query on login/register mutations + user fragment

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,16 +1,77 @@
 import { ChakraProvider } from '@chakra-ui/core';
-import { createClient, Provider } from 'urql';
-
+import { createClient, dedupExchange, fetchExchange, Provider } from 'urql';
+import { cacheExchange, Cache, QueryInput } from '@urql/exchange-graphcache';
+import {
+  LoginMutation,
+  MeDocument,
+  MeQuery,
+  RegisterMutation,
+} from '../generated/graphql';
 import theme from '../theme';
+
+function updateQueryWrapper<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, data => fn(result, data as any) as any);
+}
 
 const client = createClient({
   url: 'http://localhost:4000/graphql',
   fetchOptions: {
     credentials: 'include',
   },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (_result, args, cache, info) => {
+            updateQueryWrapper<LoginMutation, MeQuery>(
+              cache,
+              {
+                query: MeDocument,
+              },
+              _result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.login.user,
+                  };
+                }
+              }
+            );
+          },
+          register: (_result, args, cache, info) => {
+            updateQueryWrapper<RegisterMutation, MeQuery>(
+              cache,
+              {
+                query: MeDocument,
+              },
+              _result,
+              (result, query) => {
+                if (result.register.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.register.user,
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: any) {
   return (
     <Provider value={client}>
       <ChakraProvider theme={theme}>
