35a814b495f6993d92f9d9ed7151b18a5efdd0ed
Brian Alexis <51463930+brrianalexis@users.noreply.github.com>
2020-10-14 22:53:19 -0300

    feat: login component & mutation

---
+++ client/src/pages/login.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import { Formik, Form } from 'formik';
+import { Box, Button } from '@chakra-ui/core';
+import { Wrapper } from '../components/Wrapper';
+import { InputField } from '../components/InputField';
+import { useLoginMutation } from '../generated/graphql';
+import { toErrorMap } from '../utils/toErrorMap';
+
+const Login: React.FC<{}> = ({}) => {
+  const router = useRouter();
+  const [, login] = useLoginMutation();
+
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: '', password: '' }}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await login({ options: values });
+          //?   acá el optional chaining lo usamos para que en la condicion se evalúe a fondo, ya que response.data puede ser undefined
+          //?   en el caso de que data sea undefined, por el optional chaining, no va a tirar error
+          //?   sin optional chaining, tiraría error en caso de ser undefined
+          if (response.data?.login.errors) {
+            [{ field: 'username', message: 'some error' }];
+            setErrors(toErrorMap(response.data.login.errors));
+          } else if (response.data?.login.user) {
+            router.push('/');
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"
+            />
+            <Box mt={4}>
+              <InputField
+                type="password"
+                name="password"
+                placeholder="password"
+                label="Password"
+              />
+            </Box>
+            <Button
+              type="submit"
+              colorScheme="teal"
+              mt={4}
+              isLoading={isSubmitting}
+            >
+              Login
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Login;
