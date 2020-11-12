77dbb01a5d7f48e5cd7ea260169db629fbf9da71
Brian Alexis <51463930+brrianalexis@users.noreply.github.com>
2020-11-12 00:40:36 -0300

    feat: one to many & many to one relationships

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -5,8 +5,10 @@ import {
   Entity,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
+  ManyToOne,
 } from 'typeorm';
 import { ObjectType, Field } from 'type-graphql';
+import { User } from './User';
 
 @ObjectType() //?   este decorador convierte a la clase en un ObjectType de type-graphql. los decoradores pueden stackearse, esta clase es una Entity y un ObjectType a la vez
 @Entity() //?   este decorador le dice a MikroORM que esto es una entidad que corresponde a una tabla de la db.
@@ -16,14 +18,29 @@ export class Post extends BaseEntity {
   id!: number;
 
   @Field(() => String)
+  @Column()
+  title!: string;
+
+  @Field()
+  @Column()
+  text!: string;
+
+  @Field()
+  @Column({ type: 'int', default: 0 })
+  points!: number;
+
+  @Field()
+  @Column()
+  creatorId: number;
+
+  @ManyToOne(() => User, user => user.posts)
+  creator: User;
+
+  @Field(() => String)
   @CreateDateColumn()
   createdAt: Date;
 
   @Field(() => String)
   @UpdateDateColumn()
   updatedAt: Date;
-
-  @Field(() => String)
-  @Column()
-  title!: string;
 }
--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -3,10 +3,12 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 import { ObjectType, Field } from 'type-graphql';
+import { Post } from './Post';
 
 @ObjectType()
 @Entity()
@@ -16,20 +18,23 @@ export class User extends BaseEntity {
   id!: number;
 
   @Field(() => String)
-  @CreateDateColumn()
-  createdAt: Date;
-
-  @Field(() => String)
-  @UpdateDateColumn()
-  updatedAt: Date;
-
-  @Field(() => String)
   @Column({ unique: true })
   username!: string;
 
   @Field(() => String)
   @Column({ unique: true })
   email!: string;
+
+  @OneToMany(() => Post, post => post.creator)
+  posts: Post[];
+
+  @Field(() => String)
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Field(() => String)
+  @UpdateDateColumn()
+  updatedAt: Date;
 
   //?   Al no tener el decorador @Field(), no se puede seleccionar la contraseña, solo va a ser una columna en la db
   @Column()
